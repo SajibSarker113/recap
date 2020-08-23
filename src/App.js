4d6d9ade8b687510282d6c8d9b9f7d8bb8cb0131
SajibSarker113 <66987905+SajibSarker113@users.noreply.github.com>
2020-08-23 18:52:00 +0600

    33.8 EVENT HANDLER STATE UPDATE AND PASS STATE RECAP

--- src/App.js
+++ src/App.js
@@ -18,12 +18,22 @@ function App() {
 
 function MovieCounter(){
   const [count, setCount]= useState(0);
+  const handleClick = () => setCount(count + 1)
   return (
     <div>
-      <button>Add Movie</button>
-      <h3>Number of movie: </h3>
+      <button onClick={handleClick}>Add Movie</button>
+      <h3>Number of movie: {count} </h3>
+
+      <MovieDisplay movies={count}></MovieDisplay>
+      <MovieDisplay movies={count + 10}></MovieDisplay>
+      <MovieDisplay movies={count}></MovieDisplay>
+      <MovieDisplay movies={count}></MovieDisplay>
     </div>
   )
+}
+
+function MovieDisplay(props){
+  return <h4>Movies I have acted:{props.movies} </h4>
 }
 
 function Hero(props){
